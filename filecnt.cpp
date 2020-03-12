977a7540e047b60783ac1d9095cadb4733660684
Viral <viralcsanghavi@gmail.com>
2020-03-12 15:57:49 -0500

    first commit

---
+++ filecnt.cpp
@@ -0,0 +1,39 @@
+#include <iostream> // new comment added for makefile exercise
+#include <string>
+#include <filesystem>
+#include "version.cpp"
+using namespace std;
+namespace fs = std::filesystem;
+
+int main (int argc, char** argv) {
+    long directoryCounter = 0;
+    long plainFilesCounter = 0;
+    unsigned long long bytesAmount = 0;
+
+    // This will iterate through the directories recursively!
+    for (auto& fileObject : fs::recursive_directory_iterator(argv[1], fs::directory_options::skip_permission_denied)) { 
+        // try block for errors
+        try {
+            if (fileObject.is_directory()) {
+                // checks if file is a directory...
+                ++directoryCounter;  
+                // will add to counter          
+            }
+            else {
+                // else if it's not a directory, then it a plain/regular file
+                ++plainFilesCounter;
+                bytesAmount += fileObject.file_size();
+                // will add to counter and add to bytesAmount based on size of file
+            }
+        }
+        catch (const exception &e) {
+            // catch block will catch the errors ignoring permission errors
+            continue;
+        }   
+    }
+    cout << "The total number of directories in directory " << argv[1] << " is: " << directoryCounter << "." << endl << endl;
+    cout << "The total number of files in directory " << argv[1] << " is: " << plainFilesCounter << "." << endl << endl;
+    cout << "The total number of bytes occupied by all files in directory " << argv[1] << " is: " << bytesAmount << "." << endl << endl;
+
+    return 0;
+}
---
+++ filedisp.cpp
@@ -0,0 +1,38 @@
+#include <iostream> // new comment added for makefile exercise
+#include <string>
+#include <vector>
+#include "version.cpp"
+using namespace std;
+
+int main (int argc, char** argv) {
+    vector<string> allInputLines; // vector that will store the input lines that are piped in from previous program execution output    
+    string inputLine;
+
+    // while loop to get input line value from pipe
+    while (getline(cin, inputLine)) {
+        // if to check if there is a inputLine value provided
+        if (!inputLine.empty()) {
+            // now push value into vector
+            allInputLines.push_back(inputLine);
+        }    
+    }
+
+    displayVersion();
+    // check which option out of the three was given as argument for this program (i.e. -d, -f, -b)
+    string optionArgument = argv[1];
+    if (optionArgument == "-d") {
+        cout << allInputLines[0] << endl;
+    }
+    else if (optionArgument == "-f") {
+        cout << allInputLines[1] << endl;    
+    }
+    else if (optionArgument == "-b") {
+        cout << allInputLines[2] << endl;    
+    }
+    else {
+        // this else statement will catch any error case which is an invalid option given as an argument to this function
+        cout << "An invalid option argument was given. Please try again." << endl;    
+    }
+
+    return 0;
+}
